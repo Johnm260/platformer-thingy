aed9ebc8db53924d57956cd94506ffe6cb8e1ddf
Johnm260 <122632896+Johnm260@users.noreply.github.com>
2025-04-15 10:12:07 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,8 @@ io.on('connection', (socket) => {
             };
 
     console.log('A user connected:', socket.id, 'username:', players[socket.id].name, 'sprite:', players[socket.id].sprite);
-
+        var str = players[socket.id].name + "has connected!";
+        socket.emit("chatMessage", { name: "", message: str , color , isConsole: false });
     // Emit the 'init' event to the newly connected player
     socket.emit('init', { players, id: socket.id });
 
@@ -142,6 +143,8 @@ io.on('connection', (socket) => {
         console.log('user disconnected', socket.id);
         delete players[socket.id];
         socket.broadcast.emit('playerDisconnected', socket.id);
+        var str = players[socket.id].name + "has disconnected!";
+        socket.emit("chatMessage", { name: "", message: str , color , isConsole: false });
     });
 
     // Handle chat messages
